3b983841950f1c94e5a7a9ec1c055fb65af1ffa4
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-10 17:38:31 -0300

    added ConvexAppBarPackage with some changes

--- bento_challenge/lib/main.dart
+++ bento_challenge/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:bento_challenge/core/design/ui_colors.dart';
+import 'package:bento_challenge/navbar/bar.dart';
+import 'package:bento_challenge/navbar/item.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,11 +37,30 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.red,
+        color: Colors.white,
         padding: const EdgeInsets.all(16),
         child: Container(
           color: Colors.blue,
         ),
+      ),
+      bottomNavigationBar: ConvexAppBar(
+        backgroundColor: UIColors.alabaster,
+        activeColor: UIColors.shamrock,
+        color: UIColors.fringyFlower,
+        style: TabStyle.fixedCircle,
+        elevation: 5,
+        height: 80,
+        curveSize: 130,
+        top: -60,
+        items: const [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.map, title: 'Discovery'),
+          TabItem(icon: Icons.store, title: 'Add'),
+          TabItem(icon: Icons.message, title: 'Message'),
+          TabItem(icon: Icons.people, title: 'Profile'),
+        ],
+        initialActiveIndex: 2,
+        onTap: (int i) => print('click index=$i'),
       ),
     );
   }
