2b962ab3ba76a428308173f22d2aa18c8a7a2686
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-09 21:49:16 -0300

    created ui_colors

---
+++ bento_challenge/lib/core/design/ui_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class UIColors {
+  static const Color shamrock = Color(0xff56e39f);
+  static const Color blueZodiac = Color(0xff10365b);
+  static const Color alabaster = Color(0xfff7f7f7);
+  static const Color lightningYellow = Color(0xfffec024);
+  static const Color baliHai = Color(0xff879ead);
+  static const Color fringyFlower = Color(0xffaddac5);
+  static const Color beige = Color(0xfff2f6df);
+  static const Color eggSour = Color(0xfffff4de);
+}
