f7efa31c8f2ffc6aac94b986ea14c9990d762bda
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-10 20:47:17 -0300

    Update product_module.dart

--- bento_challenge/lib/product/product_module.dart
+++ bento_challenge/lib/product/product_module.dart
@@ -1,9 +1,22 @@
+import 'package:bento_challenge/core/clients/core_mocked_client.dart';
+import 'package:bento_challenge/product/data/gateways/product_gateway.dart';
+import 'package:bento_challenge/product/data/repositories/product_repository.dart';
+import 'package:bento_challenge/product/domain/services/get_product_details_by_id_service.dart';
+import 'package:bento_challenge/product/presentation/views/controllers/product_details_controller.dart';
 import 'package:bento_challenge/product/presentation/views/product_details_view.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class ProductModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    i.addSingleton<IMockedClient>(CoreMockedClient.new);
+    i.addSingleton<IProductGateway>(ProductGateway.new);
+    i.addSingleton<IProductRepository>(ProductRepository.new);
+
+    i.addSingleton<GetProductDetailsByIdService>(GetProductDetailsByIdService.new);
+
+    i.addSingleton<ProductDetailsController>(ProductDetailsController.new);
+  }
 
   @override
   void routes(r) {
