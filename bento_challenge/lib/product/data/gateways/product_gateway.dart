d73c2ab5fb57aa677bb9989846a4db7de29ec403
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-10 20:24:19 -0300

    created product gateway

---
+++ bento_challenge/lib/product/data/gateways/product_gateway.dart
@@ -0,0 +1,23 @@
+import 'package:bento_challenge/core/clients/core_mocked_client.dart';
+import 'package:bento_challenge/core/utils/gateway_handler.dart';
+import 'package:bento_challenge/product/domain/entities/product_details_entity.dart';
+import 'package:bento_challenge/product/error/exception.dart';
+
+abstract class IProductGateway {
+  Future<ProductDetailsEntity> getProductDetailsById(int id);
+}
+
+class ProductGateway extends GatewayHandler implements IProductGateway {
+  final CoreMockedClient _client;
+
+  ProductGateway(this._client);
+
+  @override
+  Future<ProductDetailsEntity> getProductDetailsById(int id) async {
+    return call(
+      requisitionCallback: () => _client.get('/product/details', queryParameters: {'id': id}),
+      onSuccessCallback: (data) => ProductDetailsEntity.fromMap(data),
+      exception: GetProductDetailsByIdException(),
+    );
+  }
+}
