cb6c23bbea24d9d3cf71850d2c00ee8dbc0022cf
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-10 20:25:45 -0300

    added dartz to the project

--- bento_challenge/lib/core/clients/core_mocked_client.dart
+++ bento_challenge/lib/core/clients/core_mocked_client.dart
@@ -1,6 +1,8 @@
 import 'package:bento_challenge/core/clients/mocked_paths.dart';
 import 'package:bento_challenge/core/utils/mocked_response.dart';
 
+const globalMillisecondsDelay = 1000;
+
 abstract class IMockedClient {
   Future<MockedResponse> get(String path, {Map<String, dynamic>? queryParameters});
 }
@@ -8,7 +10,7 @@ abstract class IMockedClient {
 class CoreMockedClient implements IMockedClient {
   @override
   Future<MockedResponse> get(String path, {Map<String, dynamic>? queryParameters}) async {
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(milliseconds: globalMillisecondsDelay));
     return MockedPaths.getDataFromPath(path);
   }
 }
