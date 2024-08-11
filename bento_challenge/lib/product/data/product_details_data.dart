636060673375aad3a05aa9ba4991b16806dd9f66
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-11 20:25:24 -0300

    created data for food kind and chagned product details data

--- bento_challenge/lib/product/data/product_details_data.dart
+++ bento_challenge/lib/product/data/product_details_data.dart
@@ -35,7 +35,42 @@ const Map<String, dynamic> productDetailsData = {
           'background_color': '#c3f0f0',
         }
       ],
-      'background_color': '#F2F2F2',
+      'background_color': '#edffe4',
+    },
+    {
+      'id': 2,
+      'name': 'Organic Fresh Green Cabbage',
+      'shop': 'Alisha Mart',
+      'price': '6.90',
+      'rating': '4.5',
+      'images_path': [
+        './lib/assets/images/orange0.png',
+      ],
+      'comparative_price': '8.15',
+      'details':
+          'More commonly known as green cabbage, the cannonball cabbage is one of the most popular cabbage varieties. It is so named for the way its leaves are wound tightly over one another, with the final product resembling a cannonball.',
+      'is_favorited': false,
+      'categories': [
+        {
+          'id': 1,
+          'name': 'Vegetarian',
+          'icon_path': './lib/assets/png_icons/leaf.png',
+          'background_color': '#c2f0c1',
+        },
+        {
+          'id': 2,
+          'name': 'Halal Food',
+          'icon_path': './lib/assets/png_icons/halal.png',
+          'background_color': '#f0f0bf',
+        },
+        {
+          'id': 3,
+          'name': 'Gluten-free',
+          'icon_path': './lib/assets/png_icons/gluten-free.png',
+          'background_color': '#c3f0f0',
+        }
+      ],
+      'background_color': '#fff4de',
     }
   ],
 };
---
+++ bento_challenge/lib/root/home/data/food_kind_data.dart
@@ -0,0 +1,29 @@
+const Map<String, dynamic> foodKindData = {
+  'kinds': [
+    {
+      'id': 1,
+      'name': 'Vegan',
+      'icon_path': './lib/assets/png_icons/vegetable.png',
+    },
+    {
+      'id': 2,
+      'name': 'Meat',
+      'icon_path': './lib/assets/png_icons/chicken.png',
+    },
+    {
+      'id': 3,
+      'name': 'Fruits',
+      'icon_path': './lib/assets/png_icons/fruit.png',
+    },
+    {
+      'id': 4,
+      'name': 'Milk',
+      'icon_path': './lib/assets/png_icons/milk.png',
+    },
+    {
+      'id': 5,
+      'name': 'Fish',
+      'icon_path': './lib/assets/png_icons/fish.png',
+    }
+  ],
+};
