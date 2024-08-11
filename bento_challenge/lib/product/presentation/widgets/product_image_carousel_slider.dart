2e95b2349eb8df592cd7610102aa9f892fe22504
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-11 10:54:33 -0300

    created ProductImageCarouselSlider

---
+++ bento_challenge/lib/product/presentation/widgets/product_image_carousel_slider.dart
@@ -0,0 +1,75 @@
+import 'package:bento_challenge/core/design/ui_colors.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class ProductImageCarouselSlider extends StatefulWidget {
+  const ProductImageCarouselSlider({
+    super.key,
+    required this.carouselController,
+    required this.list,
+  });
+
+  final CarouselSliderController carouselController;
+  final List<Widget> list;
+
+  @override
+  State<ProductImageCarouselSlider> createState() => _ProductImageCarouselSliderState();
+}
+
+class _ProductImageCarouselSliderState extends State<ProductImageCarouselSlider> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 250,
+          width: MediaQuery.of(context).size.width, // UIScale.widthDevice
+          color: Colors.red,
+          child: CarouselSlider.builder(
+            carouselController: widget.carouselController,
+            itemCount: widget.list.length,
+            itemBuilder: (context, index, realIndex) {
+              return widget.list[index];
+            },
+            options: CarouselOptions(
+              height: 400,
+              aspectRatio: 16 / 9,
+              viewportFraction: 1,
+              initialPage: 0,
+              enableInfiniteScroll: false,
+              reverse: false,
+              enlargeCenterPage: true,
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (var i = 0; i < widget.list.length; i++)
+              Padding(
+                padding: const EdgeInsets.only(right: 4),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  width: _currentIndex == i ? 25 : 6,
+                  height: 6,
+                  decoration: BoxDecoration(
+                    color: _currentIndex == i ? UIColors.shamrock : UIColors.parisWhite,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ],
+    );
+  }
+}
