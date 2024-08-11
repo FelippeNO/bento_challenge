ddbb034aeb2129465b0e7a4a5b53d01547a95c23
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-11 00:11:18 -0300

    added more consts in uipaddings and change it to static

--- bento_challenge/lib/core/design/ui_paddings.dart
+++ bento_challenge/lib/core/design/ui_paddings.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 
 class UIPaddings {
-  final EdgeInsets all16 = const EdgeInsets.all(16);
-  final EdgeInsets all8 = const EdgeInsets.all(8);
-  final EdgeInsets all4 = const EdgeInsets.all(4);
-  final EdgeInsets onlyVertical16 = const EdgeInsets.symmetric(vertical: 16);
-  final EdgeInsets onlyVertical8 = const EdgeInsets.symmetric(vertical: 8);
-  final EdgeInsets onlyVertical4 = const EdgeInsets.symmetric(vertical: 4);
-  final EdgeInsets onlyRight16 = const EdgeInsets.only(right: 16);
-  final EdgeInsets onlyRight8 = const EdgeInsets.only(right: 8);
-  final EdgeInsets onlyRight4 = const EdgeInsets.only(right: 4);
-  final EdgeInsets onlyLeft16 = const EdgeInsets.only(left: 16);
-  final EdgeInsets onlyLeft8 = const EdgeInsets.only(left: 8);
-  final EdgeInsets onlyLeft4 = const EdgeInsets.only(left: 4);
-  final EdgeInsets onlyTop16 = const EdgeInsets.only(top: 16);
-  final EdgeInsets onlyTop8 = const EdgeInsets.only(top: 8);
-  final EdgeInsets onlyTop4 = const EdgeInsets.only(top: 4);
-  final EdgeInsets onlyBottom16 = const EdgeInsets.only(bottom: 16);
-  final EdgeInsets onlyBottom8 = const EdgeInsets.only(bottom: 8);
-  final EdgeInsets onlyBottom4 = const EdgeInsets.only(bottom: 4);
+  static const EdgeInsets all16 = EdgeInsets.all(16);
+  static const EdgeInsets all8 = EdgeInsets.all(8);
+  static const EdgeInsets all4 = EdgeInsets.all(4);
+  static const EdgeInsets onlyVertical16 = EdgeInsets.symmetric(vertical: 16);
+  static const EdgeInsets onlyVertical8 = EdgeInsets.symmetric(vertical: 8);
+  static const EdgeInsets onlyVertical4 = EdgeInsets.symmetric(vertical: 4);
+  static const EdgeInsets onlyHorizontal16 = EdgeInsets.symmetric(horizontal: 16);
+  static const EdgeInsets onlyHorizontal8 = EdgeInsets.symmetric(horizontal: 8);
+  static const EdgeInsets onlyHorizontal4 = EdgeInsets.symmetric(horizontal: 4);
+  static const EdgeInsets onlyRight16 = EdgeInsets.only(right: 16);
+  static const EdgeInsets onlyRight8 = EdgeInsets.only(right: 8);
+  static const EdgeInsets onlyRight4 = EdgeInsets.only(right: 4);
+  static const EdgeInsets onlyLeft16 = EdgeInsets.only(left: 16);
+  static const EdgeInsets onlyLeft8 = EdgeInsets.only(left: 8);
+  static const EdgeInsets onlyLeft4 = EdgeInsets.only(left: 4);
+  static const EdgeInsets onlyTop16 = EdgeInsets.only(top: 16);
+  static const EdgeInsets onlyTop8 = EdgeInsets.only(top: 8);
+  static const EdgeInsets onlyTop4 = EdgeInsets.only(top: 4);
+  static const EdgeInsets onlyBottom16 = EdgeInsets.only(bottom: 16);
+  static const EdgeInsets onlyBottom8 = EdgeInsets.only(bottom: 8);
+  static const EdgeInsets onlyBottom4 = EdgeInsets.only(bottom: 4);
 }
