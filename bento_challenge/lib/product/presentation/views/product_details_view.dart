2b30200e93d004a553da6ce743c95800a23b7b30
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-11 10:54:46 -0300

    added some tests to product view

--- bento_challenge/lib/product/presentation/views/product_details_view.dart
+++ bento_challenge/lib/product/presentation/views/product_details_view.dart
@@ -2,12 +2,15 @@ import 'package:bento_challenge/core/design/ui_paddings.dart';
 import 'package:bento_challenge/core/widgets/animated_icon_two_states_button.dart';
 import 'package:bento_challenge/product/presentation/views/controllers/product_details_controller.dart';
 import 'package:bento_challenge/product/presentation/widgets/add_to_cart_button.dart';
+import 'package:bento_challenge/product/presentation/widgets/product_details_price_display.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../core/widgets/primary_back_button.dart';
 import '../widgets/product_avaliation_container.dart';
 import '../widgets/product_category_bar.dart';
+import '../widgets/product_image_carousel_slider.dart';
 
 class ProductDetailsView extends StatefulWidget {
   const ProductDetailsView({super.key});
@@ -18,6 +21,37 @@ class ProductDetailsView extends StatefulWidget {
 
 class _ProductDetailsViewState extends State<ProductDetailsView> {
   final ProductDetailsController controller = Modular.get<ProductDetailsController>();
+
+  final CarouselSliderController carouselController = CarouselSliderController();
+  final List<Widget> list = [
+    Container(
+      color: Colors.red,
+      child: const Center(
+        child: Text(
+          'text',
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    ),
+    Container(
+      color: Colors.blue,
+      child: const Center(
+        child: Text(
+          'text',
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    ),
+    Container(
+      color: Colors.green,
+      child: const Center(
+        child: Text(
+          'text',
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    )
+  ];
 
   @override
   void initState() {
@@ -50,6 +84,8 @@ class _ProductDetailsViewState extends State<ProductDetailsView> {
               ),
               const ProductCategoryBar(),
               const AddToCartButton(),
+              const ProductDetailsPriceDisplay(price: "6.90", originalPrice: "8.15"),
+              ProductImageCarouselSlider(carouselController: carouselController, list: list),
             ],
           ),
         ),
