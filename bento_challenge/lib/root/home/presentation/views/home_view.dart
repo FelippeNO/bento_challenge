62eb7b780880427580778a8bd6cfa9da4a35b168
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-12 09:27:32 -0300

    added pop scope to home view

--- bento_challenge/lib/root/home/presentation/views/home_view.dart
+++ bento_challenge/lib/root/home/presentation/views/home_view.dart
@@ -29,129 +29,132 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: UIScale.deviceTopPadding),
-        Container(
-          padding: UIPaddings.onlyHorizontal16,
-          height: 70,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  UIText(
-                    'Delivery',
-                    fontWeight: FontWeight.w800,
-                  ),
-                  ChangePlaceAnimatedWidget(),
-                ],
-              ),
-              Container(
-                height: 40,
-                width: 40,
-                decoration: const BoxDecoration(
-                  color: UIColors.fringyFlower,
-                  shape: BoxShape.circle,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return PopScope(
+      canPop: false,
+      child: Column(
+        children: [
+          SizedBox(height: UIScale.deviceTopPadding),
+          Container(
+            padding: UIPaddings.onlyHorizontal16,
+            height: 70,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                  padding: UIPaddings.onlyHorizontal16,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: AspectRatio(
-                          aspectRatio: 2.25,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: UIColors.shamrock,
-                              borderRadius: BorderRadius.circular(14),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 12),
-                      Expanded(
-                        child: AspectRatio(
-                          aspectRatio: 2.25,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: UIColors.shamrock,
-                              borderRadius: BorderRadius.circular(14),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Padding(
-                  padding: UIPaddings.onlyHorizontal16,
-                  child: AspectRatio(
-                    aspectRatio: 2.25,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: UIColors.beige,
-                        borderRadius: BorderRadius.circular(14),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 25),
-                const ShopByCategorySession(),
-                const SizedBox(height: 16),
-                const Padding(
-                  padding: UIPaddings.onlyHorizontal16,
-                  child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     UIText(
-                      'Today\'s Special',
-                      fontWeight: FontWeight.w900,
-                      textAlign: TextAlign.start,
-                      fontSize: 20,
-                    ),
-                    UIText(
-                      'See all',
+                      'Delivery',
                       fontWeight: FontWeight.w800,
-                      textAlign: TextAlign.start,
-                      fontSize: 17,
-                      color: UIColors.shamrock,
                     ),
-                  ]),
+                    ChangePlaceAnimatedWidget(),
+                  ],
                 ),
-                const SizedBox(height: 8),
-                Padding(
-                  padding: UIPaddings.onlyHorizontal16,
-                  child: GridView.count(
-                    physics: const NeverScrollableScrollPhysics(),
-                    crossAxisCount: 2,
-                    shrinkWrap: true,
-                    childAspectRatio: 0.8,
-                    mainAxisSpacing: 16,
-                    crossAxisSpacing: 16,
-                    children: [
-                      for (int i = 0; i < productDetailsData['products'].length; i++)
-                        ProductSnapshotContainer(productDetailsData['products'][i]),
-                    ],
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: const BoxDecoration(
+                    color: UIColors.fringyFlower,
+                    shape: BoxShape.circle,
                   ),
                 ),
-                SizedBox(height: 60 + UIScale.bottomDevicePadding),
               ],
             ),
           ),
-        ),
-      ],
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: UIPaddings.onlyHorizontal16,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: AspectRatio(
+                            aspectRatio: 2.25,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: UIColors.shamrock,
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        Expanded(
+                          child: AspectRatio(
+                            aspectRatio: 2.25,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: UIColors.shamrock,
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: UIPaddings.onlyHorizontal16,
+                    child: AspectRatio(
+                      aspectRatio: 2.25,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: UIColors.beige,
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  const AnimatedShopByCategorySession(),
+                  const SizedBox(height: 16),
+                  const Padding(
+                    padding: UIPaddings.onlyHorizontal16,
+                    child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                      UIText(
+                        'Today\'s Special',
+                        fontWeight: FontWeight.w900,
+                        textAlign: TextAlign.start,
+                        fontSize: 20,
+                      ),
+                      UIText(
+                        'See all',
+                        fontWeight: FontWeight.w800,
+                        textAlign: TextAlign.start,
+                        fontSize: 17,
+                        color: UIColors.shamrock,
+                      ),
+                    ]),
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: UIPaddings.onlyHorizontal16,
+                    child: GridView.count(
+                      physics: const NeverScrollableScrollPhysics(),
+                      crossAxisCount: 2,
+                      shrinkWrap: true,
+                      childAspectRatio: 0.8,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16,
+                      children: [
+                        for (int i = 0; i < productDetailsData['products'].length; i++)
+                          ProductSnapshotContainer(productDetailsData['products'][i]),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 60 + UIScale.bottomDevicePadding),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
