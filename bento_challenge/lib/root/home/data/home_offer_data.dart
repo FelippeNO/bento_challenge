ec29486d32319219c410793f452fa942d5723323
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-12 19:54:55 -0300

    created home offer data

---
+++ bento_challenge/lib/root/home/data/home_offer_data.dart
@@ -0,0 +1,31 @@
+const Map<String, dynamic> homeOfferData = {
+  'offers': [
+    {
+      'smallText': 'Top deal !',
+      'title': 'FRESH AVOCADO UP TO 15% OFF',
+      'imagePath': './lib/assets/images/avocado.png',
+      'callToActionText': 'Shop Noew',
+      'imageLayout': 'right',
+      'callToActionStyle': 'primary',
+      'backgroundColor': '#f2f6df',
+    },
+    {
+      'smallText': 'Top deal !',
+      'title': 'FRESH AVOCADO UP TO 15% OFF',
+      'imagePath': './lib/assets/images/avocado.png',
+      'callToActionText': 'Shop Noew',
+      'imageLayout': 'right',
+      'callToActionStyle': 'secondary',
+      'backgroundColor': '#edffe4',
+    },
+    {
+      'smallText': 'Top deal !',
+      'title': 'FRESH AVOCADO UP TO 15% OFF',
+      'imagePath': './lib/assets/images/avocado.png',
+      'callToActionText': 'Shop Noew',
+      'imageLayout': 'right',
+      'callToActionStyle': 'tertiary',
+      'backgroundColor': '#c3f0f0',
+    },
+  ],
+};
