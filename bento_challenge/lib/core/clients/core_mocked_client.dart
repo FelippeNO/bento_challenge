2c5b73b8c01ae266d4df3c09eadc230adeef5ed0
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-13 10:59:50 -0300

    added random dealy to reproduce async calling

--- bento_challenge/lib/core/clients/core_mocked_client.dart
+++ bento_challenge/lib/core/clients/core_mocked_client.dart
@@ -1,7 +1,7 @@
+import 'dart:math';
+
 import 'package:bento_challenge/core/clients/mocked_paths.dart';
 import 'package:bento_challenge/core/utils/mocked_response.dart';
-
-const globalMillisecondsDelay = 1000;
 
 abstract class IMockedClient {
   Future<MockedResponse> get(String path, {Map<String, dynamic>? queryParameters});
@@ -10,7 +10,14 @@ abstract class IMockedClient {
 class CoreMockedClient implements IMockedClient {
   @override
   Future<MockedResponse> get(String path, {Map<String, dynamic>? queryParameters}) async {
-    await Future.delayed(const Duration(milliseconds: globalMillisecondsDelay));
+    await Future.delayed(Duration(milliseconds: _generateRandomMillisecondsDelay()));
     return MockedPaths.getDataFromPath(path, queryParameters: queryParameters);
+  }
+
+  int _generateRandomMillisecondsDelay() {
+    final Random random = Random();
+    const int min = 300;
+    const int max = 1500;
+    return min + random.nextInt(max - min);
   }
 }
