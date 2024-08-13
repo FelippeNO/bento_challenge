b30987ad0d56c27f699a17101a2d44df470611db
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-13 09:08:11 -0300

    injected home services on root module

--- bento_challenge/lib/root/root_module.dart
+++ bento_challenge/lib/root/root_module.dart
@@ -1,6 +1,10 @@
 import 'package:bento_challenge/core/clients/core_mocked_client.dart';
 import 'package:bento_challenge/root/home/data/gateways/home_gateway.dart';
 import 'package:bento_challenge/root/home/data/repositories/home_repository.dart';
+import 'package:bento_challenge/root/home/domain/services/get_food_kinds_service.dart';
+import 'package:bento_challenge/root/home/domain/services/get_home_highlights_service.dart';
+import 'package:bento_challenge/root/home/domain/services/get_home_offers_service.dart';
+import 'package:bento_challenge/root/home/domain/services/get_home_product_sessions_service.dart';
 import 'package:bento_challenge/root/home/presentation/controllers/home_view_controller.dart';
 import 'package:bento_challenge/root/navbar/controller/my_bottom_nav_controller.dart';
 import 'package:bento_challenge/root/presentation/views/root_view.dart';
@@ -15,6 +19,10 @@ class RootModule extends Module {
     //home
     i.addSingleton<IHomeGateway>(HomeGateway.new);
     i.addSingleton<IHomeRepository>(HomeRepository.new);
+    i.addSingleton<GetFoodKindsService>(GetFoodKindsService.new);
+    i.addSingleton<GetHomeHighlightsService>(GetHomeHighlightsService.new);
+    i.addSingleton<GetHomeOffersService>(GetHomeOffersService.new);
+    i.addSingleton<GetHomeProductSessionsService>(GetHomeProductSessionsService.new);
     i.addSingleton<HomeViewController>(HomeViewController.new);
   }
 
