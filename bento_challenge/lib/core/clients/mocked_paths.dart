8447e5a01185f95ba60cf1c965ca9714114249c2
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-13 09:12:55 -0300

    updated mocked client

--- bento_challenge/lib/core/clients/mocked_paths.dart
+++ bento_challenge/lib/core/clients/mocked_paths.dart
@@ -1,5 +1,9 @@
 import 'package:bento_challenge/core/utils/mocked_response.dart';
 import 'package:bento_challenge/product/data/product_details_data.dart';
+import 'package:bento_challenge/root/home/data/food_kind_data.dart';
+import 'package:bento_challenge/root/home/data/home_highlight_data.dart';
+import 'package:bento_challenge/root/home/data/home_offer_data.dart';
+import 'package:bento_challenge/root/home/data/home_products_session_data.dart';
 
 class MockedPaths {
   static MockedResponse getDataFromPath(String path, {Map<String, dynamic>? queryParameters}) {
@@ -7,6 +11,22 @@ class MockedPaths {
       '/product/details' => MockedResponse(
           statusCode: 200,
           data: _getProductById(queryParameters),
+        ),
+      '/home/food_kinds' => MockedResponse(
+          statusCode: 200,
+          data: foodKindData,
+        ),
+      '/home/highlights' => MockedResponse(
+          statusCode: 200,
+          data: homeHighlightsData,
+        ),
+      '/home/offers' => MockedResponse(
+          statusCode: 200,
+          data: homeOfferData,
+        ),
+      '/home/product_sessions' => MockedResponse(
+          statusCode: 200,
+          data: homeProductsSessionData,
         ),
       _ => MockedResponse(
           statusCode: 404,
