60f76053e206a96c331148fcfd1074e6279e293b
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2024-08-13 09:02:57 -0300

    injected gateway and repository on root module

--- bento_challenge/lib/root/root_module.dart
+++ bento_challenge/lib/root/root_module.dart
@@ -1,3 +1,6 @@
+import 'package:bento_challenge/core/clients/core_mocked_client.dart';
+import 'package:bento_challenge/root/home/data/gateways/home_gateway.dart';
+import 'package:bento_challenge/root/home/data/repositories/home_repository.dart';
 import 'package:bento_challenge/root/home/presentation/controllers/home_view_controller.dart';
 import 'package:bento_challenge/root/navbar/controller/my_bottom_nav_controller.dart';
 import 'package:bento_challenge/root/presentation/views/root_view.dart';
@@ -6,8 +9,12 @@ import 'package:flutter_modular/flutter_modular.dart';
 class RootModule extends Module {
   @override
   void binds(i) {
+    i.addSingleton<IMockedClient>(CoreMockedClient.new);
     i.addSingleton<MyBottomNavBarController>(MyBottomNavBarController.new);
 
+    //home
+    i.addSingleton<IHomeGateway>(HomeGateway.new);
+    i.addSingleton<IHomeRepository>(HomeRepository.new);
     i.addSingleton<HomeViewController>(HomeViewController.new);
   }
 
